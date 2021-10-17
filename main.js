ab64c867f4a3788d69f42389529f7dd17aa5ee78
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-17 23:06:07 +0100

    Add myChart information for chart

---
+++ main.js
@@ -0,0 +1,29 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const labels = [
+  'Black Ops',
+  'Modern Warfare 3',
+  'Black Ops 2',
+  'Ghosts',
+  'Black Ops 3',
+];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      data: [30.72, 30.71, 29.59, 28.98, 26.72],
+      label: 'Call of Duty Sales',
+    },
+  ],
+};
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+  },
+};
+
+const myChart = new Chart(ctx, config);
