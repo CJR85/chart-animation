b7c2aa1c225bae8d99c8f992b0dc920222481d6d
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-17 23:20:48 +0100

    Add gradient color to chart

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
+// Gradient Fill
+let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(58,123,213,1)');
+gradient.addColorStop(1, 'rgba(0,210,255,0.3)');
+
 const labels = [
   'Black Ops',
   'Modern Warfare 3',
@@ -14,6 +19,8 @@ const data = {
     {
       data: [30.72, 30.71, 29.59, 28.98, 26.72],
       label: 'Call of Duty Sales',
+      fill: true,
+      backgroundColor: gradient,
     },
   ],
 };
