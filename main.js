935797dc9d5bbeae44cdbacf34e3113653326e90
Calvin Roberts <roberts.calvin85@gmail.com>
2021-10-17 23:40:46 +0100

    Add animation delay to chart

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 
+let delayed;
+
 // Gradient Fill
 let gradient = ctx.createLinearGradient(0, 0, 0, 400);
 gradient.addColorStop(0, 'rgba(58,123,213,1)');
@@ -33,7 +35,20 @@ const config = {
   options: {
     radius: 5,
     hitRadius: 30,
+    hoverRadius: 12,
     responsive: true,
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
     scales: {
       y: {
         ticks: {
